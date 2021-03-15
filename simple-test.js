89253afcf8f488042dc4e06a8f05bb767e051e72
overfloat <rillrate@gmail.com>
2021-03-15 22:57:47 +0300

    Improve simple-test

--- simple-test.js
+++ simple-test.js
@@ -1,4 +1,4 @@
-const { install, Gauge } = require('./index')
+const { install, Counter, Gauge, Pulse, Dict, Logger, Histogram } = require('./index')
 
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms))
@@ -6,8 +6,25 @@ function sleep(ms) {
 
 async function test() {
     install()
-    gauge = new Gauge("my.provider");
-    gauge.set(1.2);
+
+    counter = new Counter("my.counter");
+    counter.set(1.2);
+
+    gauge = new Gauge("my.gauge", 0, 100);
+    gauge.set(20);
+
+    pulse = new Pulse("my.pulse");
+    pulse.set(1.2);
+
+    histogram = new Histogram("my.histogram", [10, 50, 100, 200, 500, 1000]);
+    histogram.add(150);
+
+    dict = new Dict("my.dict");
+    dict.set("key", "value");
+
+    logger = new Logger("my.logger");
+    logger.log("log message");
+
     await sleep(5000)
 }
 
